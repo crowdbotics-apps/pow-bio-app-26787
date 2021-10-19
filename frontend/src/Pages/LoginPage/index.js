94c833523f6c8c533d8144e9d7e84d756bcfd2dd
Awais Khan <aawais.nu@gmail.com>
2021-10-19 17:52:39 +0500

    added login and signup pages

--- frontend/src/Pages/LoginPage/index.js
+++ frontend/src/Pages/LoginPage/index.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AuthLayout from "../../Layouts/AuthLayout";
 
 const LoginPage = () => {
-    return <h1>Login Page</h1>
+    return <AuthLayout pageTitle="Log in">
+        <form>
+            <div className="mb-3">
+                <input type="text" className="form-control" id="email" placeholder="Email" />
+            </div>
+
+            <div className="mb-3">
+                <input type="password" className="form-control" id="password" placeholder="Password" />
+            </div>
+
+            <p className="text-end">
+                <Link to="/signup">Forget Password</Link>
+            </p>
+
+            <div className="d-grid">
+                <button className="btn btn-primary" type="button">submit</button>
+            </div>
+
+            <p className="text-center mt-4">
+                Don’t have an account? <Link to="/signup">Sign up</Link>
+            </p>
+        </form>
+    </AuthLayout>
 };
 
 export default LoginPage;
--- frontend/src/Pages/SignupPage/index.js
+++ frontend/src/Pages/SignupPage/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AuthLayout from "../../Layouts/AuthLayout";
 
 const SignupPage = () => {
-    return <h1>Signup Page</h1>
+    return <AuthLayout pageTitle="Sign up">
+        <form>
+            <div className="mb-3">
+                <input type="text" className="form-control" id="email" placeholder="Full Name" />
+            </div>
+
+            <div className="mb-3">
+                <input type="text" className="form-control" id="email" placeholder="Email" />
+            </div>
+
+            <div className="mb-3">
+                <input type="password" className="form-control" id="password" placeholder="Password" />
+            </div>
+
+            <p className="text-end">
+                <Link to="/signup">Forget Password</Link>
+            </p>
+
+            <div className="d-grid">
+                <button className="btn btn-primary" type="button">submit</button>
+            </div>
+
+            <p className="text-center mt-4">
+                Already have an account? <Link to="/login">Login</Link>
+            </p>
+        </form>
+    </AuthLayout>
 };
 
 export default SignupPage;

324b7d467367133d5a884d20c6d658beee3dae60
Awais Khan <aawais.nu@gmail.com>
2021-10-20 23:34:22 +0500

    added password view button

--- frontend/src/Pages/LoginPage/index.js
+++ frontend/src/Pages/LoginPage/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PasswordField from "../../Components/PasswordField";
 import AuthLayout from "../../Layouts/AuthLayout";
 
 const LoginPage = () => {
     return <AuthLayout pageTitle="Log in">
-        <form>
+        <form className="mt-5">
             <div className="mb-3">
                 <input type="text" className="form-control" id="email" placeholder="Email" />
             </div>
 
             <div className="mb-3">
-                <input type="password" className="form-control" id="password" placeholder="Password" />
+                <PasswordField />
             </div>
 
-            <p className="text-end">
+            <p className="text-end mb-5">
                 <Link to="/signup">Forget Password</Link>
             </p>
 
--- frontend/src/Pages/SignupPage/index.js
+++ frontend/src/Pages/SignupPage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PasswordField from "../../Components/PasswordField";
 import AuthLayout from "../../Layouts/AuthLayout";
 
 const SignupPage = () => {
     return <AuthLayout pageTitle="Sign up">
-        <form>
+        <form className="mt-5">
             <div className="mb-3">
                 <input type="text" className="form-control" id="email" placeholder="Full Name" />
             </div>
@@ -13,13 +14,9 @@ const SignupPage = () => {
                 <input type="text" className="form-control" id="email" placeholder="Email" />
             </div>
 
-            <div className="mb-3">
-                <input type="password" className="form-control" id="password" placeholder="Password" />
+            <div className="mb-5">
+                <PasswordField />
             </div>
-
-            <p className="text-end">
-                <Link to="/signup">Forget Password</Link>
-            </p>
 
             <div className="d-grid">
                 <button className="btn btn-primary" type="button">submit</button>

745092ed417b70bf875e3e326201c6fa6122ec04
Awais Khan <aawais.nu@gmail.com>
2021-11-04 15:48:48 +0500

    fixed accordion issue

--- frontend/src/Pages/HomePage/Sections/FAQ/index.js
+++ frontend/src/Pages/HomePage/Sections/FAQ/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Collapse } from 'bootstrap';
-import { Heading, Paragraph, Section, Container } from "./styles";
+import { Heading, Paragraph, Section, Container, MTDiv } from "./styles";
 
 const FAQ = () => {
     return <Section>
@@ -10,48 +10,75 @@ const FAQ = () => {
                 Diam, phasellus at mi donec nisi, porttitor est, dictumst justo, ac, nunc facilisis enim consequat ac nulla sem orci nec, lorem amet sed proin pretium elementum tortor lobortis faucibus eget velit.
             </Paragraph>
 
-            <div className="mt-4">
+            <MTDiv>
                 <div className="accordion" id="faqAccordion">
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingOne">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Accordion Item #1
+                                What did say as a kid when asked: What do you want to be when you grow up?
                             </button>
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#faqAccordion">
                             <div className="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                Hendrerit quam amet vel risus placerat orci euismod quam in pellentesque venenatis ipsum phasellus sit risus, consectetur penatibus ac nibh consectetur ut risus arcu, vulputate vestibulum, cursus lectus scelerisque eros
                             </div>
                         </div>
                     </div>
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingTwo">
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                                Accordion Item #2
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                If you could change careers right this second, what would you do?
                             </button>
                         </h2>
                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faqAccordion">
                             <div className="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                Hendrerit quam amet vel risus placerat orci euismod quam in pellentesque venenatis ipsum phasellus sit risus, consectetur penatibus ac nibh consectetur ut risus arcu, vulputate vestibulum, cursus lectus scelerisque eros
                             </div>
                         </div>
                     </div>
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingThree">
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-                                Accordion Item #3
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                What did say as a kid when asked: What do you want to be when you grow up?
                             </button>
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#faqAccordion">
                             <div className="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                Hendrerit quam amet vel risus placerat orci euismod quam in pellentesque venenatis ipsum phasellus sit risus, consectetur penatibus ac nibh consectetur ut risus arcu, vulputate vestibulum, cursus lectus scelerisque eros
                             </div>
                         </div>
                     </div>
+
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingFour">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                                What did say as a kid when asked: What do you want to be when you grow up?
+                            </button>
+                        </h2>
+                        <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#faqAccordion">
+                            <div className="accordion-body">
+                                Hendrerit quam amet vel risus placerat orci euismod quam in pellentesque venenatis ipsum phasellus sit risus, consectetur penatibus ac nibh consectetur ut risus arcu, vulputate vestibulum, cursus lectus scelerisque eros
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingFive">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                                What did say as a kid when asked: What do you want to be when you grow up?
+                            </button>
+                        </h2>
+                        <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#faqAccordion">
+                            <div className="accordion-body">
+                                Hendrerit quam amet vel risus placerat orci euismod quam in pellentesque venenatis ipsum phasellus sit risus, consectetur penatibus ac nibh consectetur ut risus arcu, vulputate vestibulum, cursus lectus scelerisque eros
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-            </div>
+            </MTDiv>
         </Container>
     </Section>
 };
